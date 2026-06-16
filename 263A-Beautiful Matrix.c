af89986d301f18dcb48d3d2e5527ba49b4aa7f99
Senthil Raja R <senthilrajasen637@gmail.com>
2026-06-16 12:51:21 +0530

    feat: add solution for Codeforces 263A Beautiful Matrix

---
+++ 263A-Beautiful Matrix.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a[5][5];
+    int r,c;
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            scanf("%d",&a[i][j]);
+            if(a[i][j]==1){
+                r=i;
+                c=j;
+            }
+        }
+    }
+    printf("%d",abs(r-2)+abs(c-2));
+    return 0;
+}
